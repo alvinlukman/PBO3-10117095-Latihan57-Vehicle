29703f03ff8f3f05900aedcae088e0033502d88f
alvinlukmanNH <galinx20gmail.com>
2018-10-30 00:20:15 +0700

    vehicle

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama        : Alvin Lukman Nulhakim
+ * Nim         : 10117095
+ * Kelas       : IF-3
+ */
+public class Main {
+     public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle();
+        Skateboard skateboard = new Skateboard();
+    }
+}
